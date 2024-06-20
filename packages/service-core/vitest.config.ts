761e27130b3e62cb8ad7d30e7a75ae3ef639aee6
Steven Ontong <steven@journeyapps.com>
2024-06-20 18:23:13 +0200

    fix tests

--- packages/service-core/vitest.config.ts
+++ packages/service-core/vitest.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()]
+  plugins: [tsconfigPaths()],
+  test: {
+    setupFiles: './test/src/setup.ts'
+  }
 });
