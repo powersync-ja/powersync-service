8842f91432b3c168c26d9f8dcb8201ee3e90e065
Roland Teichert <roland@journeyapps.com>
2024-07-12 16:55:24 +0200

    Added the initial definitions for the modular replication support

---
+++ packages/service-core/src/replication/core/ReplicationAdapter.ts
@@ -0,0 +1,78 @@
+import { TablePattern } from '@powersync/service-sync-rules';
+import { ReplicationEntity } from './ReplicationEntity.js';
+import { SaveOptions } from '../../storage/BucketStorage.js';
+
+/**
+ *  Manage the creation and termination of connections for a datasource.
+ *  For some data sources there will be no difference between snapshot and replication connections
+ */
+interface ConnectionManager<TConnection> {
+  /**
+   *  Create a connection to use for replication.
+   */
+  createReplicationConnection(): Promise<TConnection>;
+
+  /**
+   *  Create a connection to use for the initial snapshot replication.
+   *  This connection should not be shared
+   */
+  createConnection(): Promise<TConnection>;
+
+  /**
+   *  Map a datasource specific connection error to a general category of powersync errors
+   *  @param error
+   */
+  mapError(error: Error): ConnectionError;
+}
+
+/**
+ * The ReplicationAdapter describes all the methods that are required by the
+ * Replicator to replicate data from a datasource into the PowerSync Bucket storage
+ */
+export interface ReplicationAdapter<TConnection> {
+  /**
+   *  Unique name to identify this adapter in the PowerSync system
+   *  Suggestion: datasource type + datasource name
+   */
+  name(): string;
+
+  /**
+   *  Return a manager that can create connections to the DataSource
+   */
+  createConnectionManager(): ConnectionManager<TConnection>;
+
+  /**
+   *  Validate any configuration on the datasource that needs to be in place for
+   *  replication. If any configuration is missing or incorrect, an error should be thrown with the details.
+   */
+  validateConfiguration(connection: TConnection): void;
+
+  /**
+   * Get all the fully qualified entities that match the provided pattern
+   * @param connection
+   * @param pattern // TODO: Need something more generic than TablePattern
+   */
+  toReplicationEntities(connection: TConnection, pattern: TablePattern): Promise<ReplicationEntity<any>[]>;
+
+  /**
+   *  Start replicating data, assumes that initializeData has already finished running
+   *  Stream any changes back on the provided changeListener
+   *  @param connection
+   *  @param changeListener
+   */
+  startReplication(connection: TConnection, changeListener: (change: SaveOptions) => {}): Promise<void>;
+
+  /**
+   *  Immediately interrupt and stop any currently running replication.
+   */
+  terminateReplication(): Promise<void>;
+}
+
+export enum ConnectionError {
+  INCORRECT_CREDENTIALS,
+  HOST_INACCESSIBLE,
+  CONNECTION_REFUSED,
+  CONNECTION_CLOSED,
+  RETRIEVAL_FAILED,
+  GENERAL
+}
---
+++ packages/service-core/src/replication/core/ReplicationModule.ts
@@ -0,0 +1,81 @@
+import { DataSourceConfig } from '@powersync/service-types/dist/config/PowerSyncConfig.js';
+import * as t from 'ts-codec';
+import { logger, schema } from '@powersync/lib-services-framework';
+import { ReplicationAdapter } from './ReplicationAdapter.js';
+import { SyncAPI } from '../../api/SyncAPI.js';
+import { AbstractModule } from '../../modules/AbstractModule.js';
+import { ServiceContext } from '../../system/ServiceContext.js';
+
+/**
+ *  A replication module describes all the functionality that PowerSync requires to
+ *  replicate data from a DataSource. Whenever a new data source is added to powersync this class should be extended.
+ */
+export abstract class ReplicationModule extends AbstractModule {
+  protected type: string;
+
+  /**
+   * @param name
+   * @param type The type of the data source
+   * @protected
+   */
+  protected constructor(name: string, type: string) {
+    super(name);
+    this.type = type;
+  }
+
+  /**
+   *  Create the API adapter for the DataSource required by the sync API
+   *  endpoints.
+   */
+  protected abstract createSyncAPIAdapter(): SyncAPI;
+
+  /**
+   *  Create the ReplicationAdapter to be used by PowerSync replicator.
+   */
+  protected abstract createReplicationAdapter(): ReplicationAdapter<any>;
+
+  /**
+   *  Return the TS codec schema describing the required configuration values for this module.
+   */
+  protected abstract configSchema(): t.ObjectCodec<any>;
+
+  /**
+   *  Register this module's replication adapters and sync API providers if the required configuration is present.
+   */
+  public async initialize(context: ServiceContext): Promise<void> {
+    if (!context.configuration.data_sources) {
+      // No data source configuration found in the config skip for now
+      // TODO: Consider a mechanism to check for config in the ENV variables as well
+      return;
+    }
+
+    const matchingConfig = context.configuration.data_sources.filter((dataSource) => dataSource.type === this.type);
+
+    if (matchingConfig.length > 1) {
+      logger.warning(
+        `Multiple data sources of type ${this.type} found in the configuration. Only the first will be used.`
+      );
+    }
+
+    try {
+      // If validation fails, log the error and continue, no replication will happen for this data source
+      this.validateConfig(matchingConfig[0]);
+      context.replicationEngine.register(this.createReplicationAdapter());
+      context.syncAPIProvider.register(this.createSyncAPIAdapter());
+    } catch (e) {
+      logger.error(e);
+    }
+  }
+
+  private validateConfig(config: DataSourceConfig): void {
+    const validator = schema
+      .parseJSONSchema(t.generateJSONSchema(this.configSchema(), { allowAdditional: true }))
+      .validator();
+
+    const valid = validator.validate(config);
+
+    if (!valid.valid) {
+      throw new Error(`Failed to validate Module ${this.name} configuration: ${valid.errors.join(', ')}`);
+    }
+  }
+}
