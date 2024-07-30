1ac8856dd794307289d2917b8a52d6da7644f5ae
Steven Ontong <steven@journeyapps.com>
2024-07-30 13:24:39 +0200

    move wal files out of core

---
+++ modules/module-postgres/src/replication/replication-index.ts
@@ -0,0 +1,6 @@
+export * from './PgRelation.js';
+export * from './replication-utils.js';
+export * from './WalConnection.js';
+export * from './WalStream.js';
+export * from './WalStreamManager.js';
+export * from './WalStreamRunner.js';
