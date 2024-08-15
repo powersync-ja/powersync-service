683e1d0e50021edf678573c710505f9707546899
Ralf Kistner <ralf@journeyapps.com>
2024-08-15 10:07:56 +0200

    Make some export options configurable.

--- packages/sync-rules/src/TsSchemaGenerator.ts
+++ packages/sync-rules/src/TsSchemaGenerator.ts
@@ -3,17 +3,24 @@ import { SchemaGenerator } from './SchemaGenerator.js';
 import { SqlSyncRules } from './SqlSyncRules.js';
 import { SourceSchema } from './types.js';
 
+export interface TsSchemaGeneratorOptions {
+  language?: 'ts' | 'js';
+  imports?: 'web' | 'react-native' | 'auto';
+}
 export class TsSchemaGenerator extends SchemaGenerator {
   readonly key = 'ts';
   readonly label = 'TypeScript';
   readonly mediaType = 'application/typescript';
   readonly fileName = 'schema.ts';
 
+  constructor(public readonly options: TsSchemaGeneratorOptions = {}) {
+    super();
+  }
+
   generate(source: SqlSyncRules, schema: SourceSchema): string {
     const tables = super.getAllTables(source, schema);
 
-    return `import { column, Schema, TableV2 } from '@powersync/web';
-// OR: import { column, Schema, TableV2 } from '@powersync/react-native';
+    return `${this.generateImports()}
 
 ${tables.map((table) => this.generateTable(table.name, table.columns)).join('\n\n')}
 
@@ -21,8 +28,28 @@ export const AppSchema = new Schema({
   ${tables.map((table) => table.name).join(',\n  ')}
 });
 
-export type Database = (typeof AppSchema)['types'];
-`;
+${this.generateTypeExports()}`;
+  }
+
+  private generateTypeExports() {
+    const lang = this.options.language ?? 'ts';
+    if (lang == 'ts') {
+      return `export type Database = (typeof AppSchema)['types'];\n`;
+    } else {
+      return ``;
+    }
+  }
+
+  private generateImports() {
+    const importStyle = this.options.imports ?? 'auto';
+    if (importStyle == 'web') {
+      return `import { column, Schema, TableV2 } from '@powersync/web';`;
+    } else if (importStyle == 'react-native') {
+      return `import { column, Schema, TableV2 } from '@powersync/react-native';`;
+    } else {
+      return `import { column, Schema, TableV2 } from '@powersync/web';
+// OR: import { column, Schema, TableV2 } from '@powersync/react-native';`;
+    }
   }
 
   private generateTable(name: string, columns: ColumnDefinition[]): string {
