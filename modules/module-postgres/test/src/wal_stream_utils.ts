2f0442329c986d07cd1397e6171593492fbc9bd3
Ralf Kistner <ralf@journeyapps.com>
2024-12-05 15:02:00 +0200

    Test resuming of intial replication.

--- modules/module-postgres/test/src/wal_stream_utils.ts
+++ modules/module-postgres/test/src/wal_stream_utils.ts
@@ -1,9 +1,10 @@
 import { fromAsync } from '@core-tests/stream_utils.js';
 import { PgManager } from '@module/replication/PgManager.js';
 import { PUBLICATION_NAME, WalStream, WalStreamOptions } from '@module/replication/WalStream.js';
-import { BucketStorageFactory, SyncRulesBucketStorage } from '@powersync/service-core';
+import { BucketStorageFactory, OplogEntry, SyncRulesBucketStorage } from '@powersync/service-core';
 import * as pgwire from '@powersync/service-jpgwire';
 import { clearTestDb, getClientCheckpoint, TEST_CONNECTION_OPTIONS } from './util.js';
+import { StorageOptions } from '@core-tests/util.js';
 
 export class WalStreamTestContext implements AsyncDisposable {
   private _walStream?: WalStream;
@@ -18,11 +19,16 @@ export class WalStreamTestContext implements AsyncDisposable {
    *
    * This configures all the context, and tears it down afterwards.
    */
-  static async open(factory: () => Promise<BucketStorageFactory>) {
-    const f = await factory();
+  static async open(
+    factory: (options: StorageOptions) => Promise<BucketStorageFactory>,
+    options?: { doNotClear?: boolean }
+  ) {
+    const f = await factory({ doNotClear: options?.doNotClear });
     const connectionManager = new PgManager(TEST_CONNECTION_OPTIONS, {});
 
-    await clearTestDb(connectionManager.pool);
+    if (!options?.doNotClear) {
+      await clearTestDb(connectionManager.pool);
+    }
 
     return new WalStreamTestContext(f, connectionManager);
   }
@@ -33,6 +39,10 @@ export class WalStreamTestContext implements AsyncDisposable {
   ) {}
 
   async [Symbol.asyncDispose]() {
+    await this.dispose();
+  }
+
+  async dispose() {
     this.abortController.abort();
     await this.streamPromise;
     await this.connectionManager.destroy();
@@ -53,6 +63,16 @@ export class WalStreamTestContext implements AsyncDisposable {
 
   async updateSyncRules(content: string) {
     const syncRules = await this.factory.updateSyncRules({ content: content });
+    this.storage = this.factory.getInstance(syncRules);
+    return this.storage!;
+  }
+
+  async loadNextSyncRules() {
+    const syncRules = await this.factory.getNextSyncRulesContent();
+    if (syncRules == null) {
+      throw new Error(`Next sync rules not available`);
+    }
+
     this.storage = this.factory.getInstance(syncRules);
     return this.storage!;
   }
@@ -112,9 +132,18 @@ export class WalStreamTestContext implements AsyncDisposable {
     start ??= '0';
     const checkpoint = await this.getCheckpoint(options);
     const map = new Map<string, string>([[bucket, start]]);
-    const batch = this.storage!.getBucketDataBatch(checkpoint, map);
-    const batches = await fromAsync(batch);
-    return batches[0]?.batch.data ?? [];
+    let data: OplogEntry[] = [];
+    while (true) {
+      const batch = this.storage!.getBucketDataBatch(checkpoint, map);
+
+      const batches = await fromAsync(batch);
+      data = data.concat(batches[0]?.batch.data ?? []);
+      if (batches.length == 0 || !batches[0]!.batch.has_more) {
+        break;
+      }
+      map.set(bucket, batches[0]!.batch.next_after);
+    }
+    return data;
   }
 
   /**
