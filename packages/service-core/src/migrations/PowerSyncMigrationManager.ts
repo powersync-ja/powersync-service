2f5815d6df186b60f0a4f6af4c54aea3640caae4
stevensJourney <steven@journeyapps.com>
2024-12-19 17:30:24 +0200

    fixes from testing

--- packages/service-core/src/migrations/PowerSyncMigrationManager.ts
+++ packages/service-core/src/migrations/PowerSyncMigrationManager.ts
@@ -23,8 +23,7 @@ export abstract class AbstractPowerSyncMigrationAgent extends framework.Abstract
     const migrationsDir = this.getInternalScriptsDir();
     const files = await fs.readdir(migrationsDir);
     const migrations = files.filter((file) => {
-      // Vitest resolves ts files
-      return ['.js', '.ts'].includes(path.extname(file));
+      return '.js' == path.extname(file);
     });
 
     return await Promise.all(
