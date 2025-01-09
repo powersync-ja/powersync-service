3e9d64656e3064d9c6438aec2815bd8ad10a05d5
Ralf Kistner <ralf@journeyapps.com>
2025-01-09 12:23:53 +0200

    Update error checks.

--- packages/rsocket-router/tests/src/requests.test.ts
+++ packages/rsocket-router/tests/src/requests.test.ts
@@ -117,7 +117,7 @@ describe('Requests', () => {
     );
 
     // Should be a validation error
-    expect(JSON.stringify(spy.mock.calls[0])).includes('AUTHORIZATION');
+    expect(JSON.stringify(spy.mock.calls[0])).includes('PSYNC_S2101');
   });
 
   it('should get invalid route error', async () => {
@@ -130,6 +130,6 @@ describe('Requests', () => {
     await handleRoute(path, [], responder);
 
     // Should be a validation error
-    expect(JSON.stringify(spy.mock.calls[0])).includes('No route');
+    expect(JSON.stringify(spy.mock.calls[0])).includes('PSYNC_S2002');
   });
 });
