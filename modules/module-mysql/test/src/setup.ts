5dc75993dfc707e45c63b2680bacc3d947c334ce
Roland Teichert <roland@journeyapps.com>
2025-03-03 18:24:23 +0200

    Remove unused import

--- modules/module-mysql/test/src/setup.ts
+++ modules/module-mysql/test/src/setup.ts
@@ -1,5 +1,5 @@
 import { container } from '@powersync/lib-services-framework';
-import { METRICS_HELPER, test_utils } from '@powersync/service-core-tests';
+import { METRICS_HELPER } from '@powersync/service-core-tests';
 import { beforeAll, beforeEach } from 'vitest';
 
 beforeAll(async () => {
