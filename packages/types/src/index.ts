04b3d4f442052c9cd5faedee00f140efdbedc119
Roland Teichert <roland@journeyapps.com>
2025-03-07 12:39:50 +0200

    Export metric types as named export

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,4 +2,4 @@ export * as configFile from './config/PowerSyncConfig.js';
 
 export * from './definitions.js';
 export * as internal_routes from './routes.js';
-export * from './metrics.js';
+export * as metric_types from './metrics.js';
