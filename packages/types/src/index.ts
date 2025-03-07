60801d91a82e4fb80205014bd888b86fd9c308e6
Roland Teichert <roland@journeyapps.com>
2025-03-07 13:01:43 +0200

    Keep original metrics type export as well

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,4 +2,5 @@ export * as configFile from './config/PowerSyncConfig.js';
 
 export * from './definitions.js';
 export * as internal_routes from './routes.js';
+export * from './metrics.js';
 export * as metric_types from './metrics.js';
