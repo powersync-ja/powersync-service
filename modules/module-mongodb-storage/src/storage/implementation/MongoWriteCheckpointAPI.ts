c977332e7c0a5854a9a110d417ccb20d0e310b89
Ralf Kistner <ralf@journeyapps.com>
2025-03-14 16:48:16 +0200

    Use a single shared changestream.

--- modules/module-mongodb-storage/src/storage/implementation/MongoWriteCheckpointAPI.ts
+++ modules/module-mongodb-storage/src/storage/implementation/MongoWriteCheckpointAPI.ts
@@ -1,8 +1,12 @@
 import * as framework from '@powersync/lib-services-framework';
-import { storage, WatchUserWriteCheckpointOptions } from '@powersync/service-core';
+import { IterableSource, storage, WatchUserWriteCheckpointOptions } from '@powersync/service-core';
 import { PowerSyncMongo } from './db.js';
 import { CustomWriteCheckpointDocument, WriteCheckpointDocument } from './models.js';
 import { mongo } from '@powersync/lib-service-mongodb';
+import { AbortError } from 'ix/aborterror.js';
+import { AsyncSink } from 'ix/asynciterable/asynciterablex.js';
+
+import { wrapWithAbort } from 'ix/asynciterable/operators/withabort.js';
 
 export type MongoCheckpointAPIOptions = {
   db: PowerSyncMongo;
@@ -106,38 +110,83 @@ export class MongoWriteCheckpointAPI implements storage.WriteCheckpointAPI {
     }
   }
 
-  async *watchManagedWriteCheckpoint(
-    options: WatchUserWriteCheckpointOptions
-  ): AsyncIterable<storage.WriteCheckpointResult> {
-    // TODO: Share a single changestream across all users
+  private sharedIter = new FilteredIterable<WriteCheckpointDocument>((signal) => {
+    const clusterTimePromise = (async () => {
+      const hello = await this.db.db.command({ hello: 1 });
+      // Note: This is not valid on sharded clusters.
+      const startClusterTime = hello.lastWrite?.majorityOpTime?.ts as mongo.Timestamp;
+      startClusterTime;
+      return startClusterTime;
+    })();
 
-    let doc = null as WriteCheckpointDocument | null;
-    let clusterTime = null as mongo.Timestamp | null;
+    return {
+      iterator: this.watchAllManagedWriteCheckpoints(clusterTimePromise, signal),
+      getFirstValue: async (user_id: string) => {
+        // Potential race conditions we cater for:
 
-    await this.db.client.withSession(async (session) => {
-      doc = await this.db.write_checkpoints.findOne(
-        {
-          user_id: user_id
-        },
-        {
-          session
+        // Case 1: changestream is behind.
+        // We get a doc now, then the same or older doc again later.
+        // No problem!
+
+        // Case 2: Query is behind. I.e. doc has been created, and emitted on the changestream, but the query doesn't see it yet.
+        // Not possible luckily, but can we make sure?
+
+        // Case 3: changestream delays openeing. A doc is created after our query here, but before the changestream is opened.
+        // Awaiting clusterTimePromise should be sufficient here, but as a sanity check we also confirm that our query
+        // timestamp is > the startClusterTime.
+
+        const changeStreamStart = await clusterTimePromise;
+
+        let doc = null as WriteCheckpointDocument | null;
+        let clusterTime = null as mongo.Timestamp | null;
+
+        await this.db.client.withSession(async (session) => {
+          doc = await this.db.write_checkpoints.findOne(
+            {
+              user_id: user_id
+            },
+            {
+              session
+            }
+          );
+          const time = session.clusterTime?.clusterTime ?? null;
+          clusterTime = time;
+        });
+        if (clusterTime == null) {
+          throw new framework.ServiceAssertionError('Could not get clusterTime for write checkpoint');
         }
-      );
-      const time = session.clusterTime?.clusterTime ?? null;
-      clusterTime = time;
-    });
-    if (clusterTime == null) {
-      throw new framework.ServiceAssertionError('Could not get clusterTime');
-    }
 
-    const { user_id, signal } = options;
+        if (clusterTime.lessThan(changeStreamStart)) {
+          throw new framework.ServiceAssertionError(
+            'clusterTime for write checkpoint is older than changestream start'
+          );
+        }
+
+        return doc;
+      }
+    };
+  });
+
+  private async *watchAllManagedWriteCheckpoints(
+    clusterTimePromise: Promise<mongo.BSON.Timestamp>,
+    signal: AbortSignal
+  ): AsyncGenerator<FilterIterableValue<WriteCheckpointDocument>> {
+    const clusterTime = await clusterTimePromise;
+
     const stream = this.db.write_checkpoints.watch(
-      [{ $match: { 'fullDocument.user_id': user_id, operationType: { $in: ['insert', 'update', 'replace'] } } }],
+      [{ $match: { operationType: { $in: ['insert', 'update', 'replace'] } } }],
       {
         fullDocument: 'updateLookup',
         startAtOperationTime: clusterTime
       }
     );
+
+    const hello = await this.db.db.command({ hello: 1 });
+    // Note: This is not valid on sharded clusters.
+    const startClusterTime = hello.lastWrite?.majorityOpTime?.ts as mongo.Timestamp;
+    if (startClusterTime == null) {
+      throw new framework.ServiceAssertionError('Could not get clusterTime');
+    }
 
     signal.onabort = () => {
       stream.close();
@@ -148,27 +197,34 @@ export class MongoWriteCheckpointAPI implements storage.WriteCheckpointAPI {
       return;
     }
 
-    let lastId = -1n;
-
-    if (doc != null) {
-      yield {
-        id: doc.client_id,
-        lsn: doc.lsns['1']
-      };
-      lastId = doc.client_id;
-    }
-
     for await (let event of stream) {
       if (!('fullDocument' in event) || event.fullDocument == null) {
         continue;
       }
+
+      const user_id = event.fullDocument.user_id;
+      yield {
+        key: user_id,
+        value: event.fullDocument
+      };
+    }
+  }
+
+  async *watchManagedWriteCheckpoint(
+    options: WatchUserWriteCheckpointOptions
+  ): AsyncIterable<storage.WriteCheckpointResult> {
+    const stream = this.sharedIter.subscribe(options.user_id, options.signal);
+
+    let lastId = -1n;
+
+    for await (let doc of stream) {
       // Guard against out-of-order events
-      if (event.fullDocument.client_id > lastId) {
+      if (doc.client_id > lastId) {
         yield {
-          id: event.fullDocument.client_id,
-          lsn: event.fullDocument.lsns['1']
+          id: doc.client_id,
+          lsn: doc.lsns['1']
         };
-        lastId = event.fullDocument.client_id;
+        lastId = doc.client_id;
       }
     }
   }
@@ -296,4 +352,137 @@ export async function batchCreateCustomWriteCheckpoints(
     })),
     {}
   );
+}
+
+export interface FilterIterableValue<T> {
+  key: string;
+  value: T;
+}
+
+interface FilteredIterableSource<T> {
+  iterator: AsyncIterable<FilterIterableValue<T>>;
+  getFirstValue(key: string): Promise<T | null>;
+}
+
+type FilteredIterableSourceFactory<T> = (signal: AbortSignal) => FilteredIterableSource<T>;
+
+export class FilteredIterable<T> {
+  private subscribers: Map<string, Set<AsyncSink<T>>> | undefined = undefined;
+  private abortController: AbortController | undefined = undefined;
+  private currentSource: FilteredIterableSource<T> | undefined = undefined;
+
+  constructor(private source: FilteredIterableSourceFactory<T>) {}
+
+  private start(filter: string, sink: AsyncSink<T>) {
+    const abortController = new AbortController();
+    const listeners = new Map();
+    listeners.set(filter, new Set([sink]));
+
+    this.abortController = abortController;
+    this.subscribers = listeners;
+
+    const source = this.source(abortController.signal);
+    this.currentSource = source;
+    this.loop(source, abortController, listeners);
+    return source;
+  }
+
+  private async loop(
+    source: FilteredIterableSource<T>,
+    abortController: AbortController,
+    sinks: Map<string, Set<AsyncSink<T>>>
+  ) {
+    try {
+      for await (let doc of source.iterator) {
+        if (abortController.signal.aborted || sinks.size == 0) {
+          throw new AbortError();
+        }
+        const key = doc.key;
+        const keySinks = sinks.get(key);
+        if (keySinks == null) {
+          continue;
+        }
+
+        for (let sink of keySinks) {
+          sink.write(doc.value);
+        }
+      }
+
+      // End of stream
+      for (let keySinks of sinks.values()) {
+        for (let sink of keySinks) {
+          sink.end();
+        }
+      }
+    } catch (e) {
+      // Just in case the error is not from the source
+      abortController.abort();
+
+      for (let keySinks of sinks.values()) {
+        for (let sink of keySinks) {
+          sink.error(e);
+        }
+      }
+    } finally {
+      // Clear state, so that a new subscription may be started
+      if (this.subscribers === sinks) {
+        this.subscribers = undefined;
+        this.abortController = undefined;
+        this.currentSource = undefined;
+      }
+    }
+  }
+
+  private removeSink(key: string, sink: AsyncSink<T>) {
+    const existing = this.subscribers?.get(key);
+    if (existing == null) {
+      return;
+    }
+    existing.delete(sink);
+    if (existing.size == 0) {
+      this.subscribers!.delete(key);
+    }
+
+    if (this.subscribers?.size == 0) {
+      // This is not immediate - there may be a delay until it is fully stopped,
+      // depending on the underlying source.
+      this.abortController?.abort();
+      this.subscribers = undefined;
+      this.abortController = undefined;
+      this.currentSource = undefined;
+    }
+  }
+
+  private addSink(key: string, sink: AsyncSink<T>) {
+    if (this.currentSource == null) {
+      return this.start(key, sink);
+    } else {
+      const existing = this.subscribers!.get(key);
+      if (existing != null) {
+        existing.add(sink);
+      } else {
+        this.subscribers!.set(key, new Set([sink]));
+      }
+      return this.currentSource;
+    }
+  }
+
+  async *subscribe(key: string, signal: AbortSignal): AsyncIterable<T> {
+    const sink = new AsyncSink<T>();
+    // Important that we register the sink before calling getFirstValue().
+    const source = this.addSink(key, sink);
+    try {
+      const firstValue = await source.getFirstValue(key);
+      if (firstValue != null) {
+        yield firstValue;
+      }
+      yield* wrapWithAbort(sink, signal);
+    } finally {
+      this.removeSink(key, sink);
+    }
+  }
+
+  get active() {
+    return this.subscribers != null;
+  }
 }
