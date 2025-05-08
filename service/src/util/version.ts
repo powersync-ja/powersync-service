eb5e797b144c38b8478db124e5b47feac7ed552d
stevensJourney <steven@journeyapps.com>
2025-05-08 10:26:05 +0200

    update Open edition tag

--- service/src/util/version.ts
+++ service/src/util/version.ts
@@ -4,6 +4,6 @@ import pkg from '@powersync/service-core/package.json' with { type: 'json' };
 
 export function logBooting(runner: string) {
   const version = pkg.version;
-  const edition = 'Community Edition';
+  const edition = 'Open Edition';
   logger.info(`Booting PowerSync Service v${version}, ${runner}, ${edition}`, { version, edition, runner });
 }
