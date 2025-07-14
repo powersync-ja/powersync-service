ef6cfd11a282790055a8f299a1be961001550e1c
JuanB <juan@journeyapps.com>
2025-07-14 15:40:15 +0200

    controller addition handler

--- packages/types/src/events.ts
+++ packages/types/src/events.ts
@@ -3,7 +3,7 @@ export enum EmitterEngineEventNames {
   SDK_DISCONNECT_EVENT = 'sdk-disconnect-event'
 }
 
-export type EventHandlerFunc = (data: any) => Promise<void>;
+export type EventHandlerFunc = (data: any) => Promise<void> | ((controller: any) => (data: any) => void);
 export interface EmitterEvent {
   name: EmitterEngineEventNames;
   handler: EventHandlerFunc;
