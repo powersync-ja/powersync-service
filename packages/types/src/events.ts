65c2f5b6f5602e5508e4e087de54b837528a06a7
JuanB <juan@journeyapps.com>
2025-07-14 15:56:09 +0200

    type fix

--- packages/types/src/events.ts
+++ packages/types/src/events.ts
@@ -3,7 +3,9 @@ export enum EmitterEngineEventNames {
   SDK_DISCONNECT_EVENT = 'sdk-disconnect-event'
 }
 
-export type EventHandlerFunc = (data: any) => Promise<void> | ((controller: any) => (data: any) => void);
+export type EventHandlerFunc =
+  | ((data: any) => Promise<void> | void)
+  | ((controller: any) => (data: any) => Promise<void> | void);
 export interface EmitterEvent {
   name: EmitterEngineEventNames;
   handler: EventHandlerFunc;
