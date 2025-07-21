9a4094eeea9ff09450c08279075ccd936ce4fa40
JuanB <juan@journeyapps.com>
2025-07-21 13:02:06 +0200

    fix

--- packages/service-core/src/storage/ReportStorageFactory.ts
+++ packages/service-core/src/storage/ReportStorageFactory.ts
@@ -1,13 +1,14 @@
 import {
   InstanceRequest,
   ListCurrentConnectionsResponse,
-  SdkConnectDocument
+  SdkConnectDocument,
+  SdkUserData
 } from '@powersync/service-types/dist/events.js';
 
 // Interface for the ReportStorageFactory
 export interface ReportStorageFactory extends AsyncDisposable {
   reportSdkConnect(data: SdkConnectDocument): Promise<void>;
-  reportSdkDisconnect(data: SdkConnectDocument): Promise<void>;
+  reportSdkDisconnect(data: SdkUserData): Promise<void>;
   listCurrentConnections(data: InstanceRequest): Promise<ListCurrentConnectionsResponse>;
   scrapeSdkData(data: InstanceRequest): Promise<ListCurrentConnectionsResponse>;
 }
