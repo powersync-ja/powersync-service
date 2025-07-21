bb9dfd7bf0df4b50b4cd18733095cb005d821dda
JuanB <juan@journeyapps.com>
2025-07-21 12:55:16 +0200

    chnaged disconnect report to use userdata

--- modules/module-mongodb-storage/src/storage/MongoReportStorage.ts
+++ modules/module-mongodb-storage/src/storage/MongoReportStorage.ts
@@ -3,7 +3,11 @@ import { storage } from '@powersync/service-core';
 import { event_types } from '@powersync/service-types';
 import { PowerSyncMongo } from './implementation/db.js';
 import { SdkConnectDocument } from './implementation/models.js';
-import { ListCurrentConnections, ListCurrentConnectionsResponse } from '@powersync/service-types/dist/events.js';
+import {
+  ListCurrentConnections,
+  ListCurrentConnectionsResponse,
+  SdkUserData
+} from '@powersync/service-types/dist/events.js';
 
 export class MongoReportStorage implements storage.ReportStorageFactory {
   private readonly client: mongo.MongoClient;
@@ -53,7 +57,7 @@ export class MongoReportStorage implements storage.ReportStorageFactory {
       }
     );
   }
-  async reportSdkDisconnect(data: SdkConnectDocument): Promise<void> {
+  async reportSdkDisconnect(data: SdkUserData): Promise<void> {
     await this.db.sdk_report_events.findOneAndDelete({ user_id: data.user_id, client_id: data.client_id });
   }
   async listCurrentConnections(data: event_types.InstanceRequest): Promise<ListCurrentConnectionsResponse> {
