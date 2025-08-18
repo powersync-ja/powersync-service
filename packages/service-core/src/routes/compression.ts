ef0e1bd42bb41ba3b15b434b00a49971e50209e2
Ralf Kistner <ralf@journeyapps.com>
2025-08-18 15:22:34 +0200

    Enable gzip, zstd compression for sync streams.

---
+++ packages/service-core/src/routes/compression.ts
@@ -0,0 +1,54 @@
+import { Readable } from 'node:stream';
+import type Negotiator from 'negotiator';
+import * as zlib from 'node:zlib';
+
+/**
+ * Compress a streamed response.
+ *
+ * `@fastify/compress` can do something similar, but does not appear to work as well on streamed responses.
+ * The manual implementation is simple enough, and gives us more control over the low-level details.
+ *
+ * @param negotiator Negotiator from the request, to negotiate response encoding
+ * @param stream plain-text stream
+ * @returns
+ */
+export function maybeCompressResponseStream(
+  negotiator: Negotiator,
+  stream: Readable
+): { stream: Readable; encodingHeaders: Record<string, string> } {
+  const encoding = (negotiator as any).encoding(['identity', 'gzip', 'zstd'], { preferred: 'identity' });
+  if (encoding == 'zstd') {
+    return {
+      stream: stream.pipe(
+        // Available since Node v23.8.0, v22.15.0
+        // This does the actual compression in a background thread pool.
+        zlib.createZstdCompress({
+          // We need to flush the frame after every new input chunk, to avoid delaying data
+          // in the output stream.
+          flush: zlib.constants.ZSTD_e_flush,
+          params: {
+            // Default compression level is 3. We reduce this slightly to limit CPU overhead
+            [zlib.constants.ZSTD_c_compressionLevel]: 2
+          }
+        })
+      ),
+      encodingHeaders: { 'Content-Encoding': 'zstd' }
+    };
+  } else if (encoding == 'gzip') {
+    return {
+      stream: stream.pipe(
+        zlib.createGzip({
+          // We need to flush the frame after every new input chunk, to avoid delaying data
+          // in the output stream.
+          flush: zlib.constants.Z_SYNC_FLUSH
+        })
+      ),
+      encodingHeaders: { 'Content-Encoding': 'gzip' }
+    };
+  } else {
+    return {
+      stream: stream,
+      encodingHeaders: {}
+    };
+  }
+}
