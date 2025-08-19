fbb6e85389195836f08c6394d802dc33557f9d16
Simon Binder <simon@journeyapps.com>
2025-08-19 11:42:06 +0200

    Rename to CustomSqliteValue

--- modules/module-mongodb/test/src/mongo_test.test.ts
+++ modules/module-mongodb/test/src/mongo_test.test.ts
@@ -5,8 +5,7 @@ import {
   CompatibilityEdition,
   SqliteInputRow,
   SqlSyncRules,
-  TimeValue,
-  CustomArray
+  TimeValue
 } from '@powersync/service-sync-rules';
 import { describe, expect, test } from 'vitest';
 
