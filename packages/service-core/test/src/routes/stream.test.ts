256b95a375b8a78b12926d518fb29e6f77abd045
Ralf Kistner <ralf@journeyapps.com>
2025-08-25 16:58:34 +0200

    Semi-functional test.

--- packages/service-core/test/src/routes/stream.test.ts
+++ packages/service-core/test/src/routes/stream.test.ts
@@ -1,37 +1,18 @@
-import { BasicRouterRequest, Context, MetricsEngine, storage, StorageEngine, SyncContext } from '@/index.js';
+import { BasicRouterRequest, Context, SyncRulesBucketStorage } from '@/index.js';
 import { logger, RouterResponse, ServiceError } from '@powersync/lib-services-framework';
+import { SqlSyncRules } from '@powersync/service-sync-rules';
+import { Readable, Writable } from 'stream';
+import { pipeline } from 'stream/promises';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { syncStreamed } from '../../../src/routes/endpoints/sync-stream.js';
-import { SqlSyncRules } from '@powersync/service-sync-rules';
+import { mockServiceContext } from './mocks.js';
 
 describe('Stream Route', () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
-  });
-
   describe('compressed stream', () => {
     it('handles missing sync rules', async () => {
-      const storageEngine: StorageEngine = {
-        activeBucketStorage: {
-          async getActiveStorage() {
-            return null;
-          }
-        } as any
-      } as any;
       const context: Context = {
         logger: logger,
-        service_context: {
-          syncContext: new SyncContext({ maxBuckets: 1, maxDataFetchConcurrency: 1, maxParameterQueryResults: 1 }),
-          routerEngine: {} as any,
-          storageEngine,
-          metricsEngine: new MetricsEngine({ disable_telemetry_sharing: true, factory: null as any }),
-          // Not used
-          configuration: null as any,
-          lifeCycleEngine: null as any,
-          migrations: null as any,
-          replicationEngine: null as any,
-          serviceMode: null as any
-        }
+        service_context: mockServiceContext(null)
       };
 
       const request: BasicRouterRequest = {
@@ -48,50 +29,56 @@ describe('Stream Route', () => {
       expect(error.errorData.code).toEqual('PSYNC_S2302');
     });
 
-    it('handles a stream error', async () => {
-      const storageEngine: StorageEngine = {
-        activeBucketStorage: {
-          async getActiveStorage() {
-            return {
-              getParsedSyncRules() {
-                return new SqlSyncRules('bucket_definitions: {}');
-              }
-            };
-          }
+    it('handles a stream error with compression', async () => {
+      // This primarily tests that an underlying storage error doesn't result in an uncaught error
+      // when compressing the stream.
+
+      const storage = {
+        getParsedSyncRules() {
+          return new SqlSyncRules('bucket_definitions: {}');
+        },
+        watchCheckpointChanges: async function* (options) {
+          throw new Error('Simulated storage error');
         }
-      } as any;
+      } as Partial<SyncRulesBucketStorage>;
+      const serviceContext = mockServiceContext(storage);
+
       const context: Context = {
         logger: logger,
-        service_context: {
-          syncContext: new SyncContext({ maxBuckets: 1, maxDataFetchConcurrency: 1, maxParameterQueryResults: 1 }),
-          routerEngine: {
-            getAPI() {
-              return {
-                getParseSyncRulesOptions() {
-                  return { defaultSchema: 'public' };
-                }
-              } as any;
-            }
-          } as any,
-          storageEngine,
-          metricsEngine: new MetricsEngine({ disable_telemetry_sharing: true, factory: null as any }),
-          // Not used
-          configuration: null as any,
-          lifeCycleEngine: null as any,
-          migrations: null as any,
-          replicationEngine: null as any,
-          serviceMode: null as any
+        service_context: serviceContext,
+        token_payload: {
+          exp: new Date().getTime() / 1000 + 10000,
+          iat: new Date().getTime() / 1000 - 10000,
+          sub: 'test-user'
         }
       };
 
+      // It may be worth eventually doing this via Fastify to test the full stack
+
       const request: BasicRouterRequest = {
-        headers: {},
+        headers: {
+          'accept-encoding': 'gzip'
+        },
         hostname: '',
         protocol: 'http'
       };
 
       const response = await (syncStreamed.handler({ context, params: {}, request }) as Promise<RouterResponse>);
       expect(response.status).toEqual(200);
+      const stream = response.data as Readable;
+      const r = await drainWithTimeout(stream).catch((error) => error);
+      expect(r.message).toContain('Simulated storage error');
     });
   });
 });
+
+export async function drainWithTimeout(readable: Readable, ms = 2_000) {
+  const devNull = new Writable({
+    write(_chunk, _enc, cb) {
+      cb();
+    } // discard everything
+  });
+
+  // Throws AbortError if it takes longer than ms, and destroys the stream
+  await pipeline(readable, devNull, { signal: AbortSignal.timeout(ms) });
+}
