1a048229be790b61cc66b03107d5f3ac52c99019
JuanB <juan@journeyapps.com>
2025-08-27 10:09:34 +0200

    removed as utc

--- modules/module-postgres-storage/src/storage/PostgresReportStorageFactory.ts
+++ modules/module-postgres-storage/src/storage/PostgresReportStorageFactory.ts
@@ -66,7 +66,7 @@ export class PostgresReportStorageFactory implements storage.ReportStorage {
             connection_report_events
           WHERE
             disconnected_at IS NULL
-            AND jwt_exp > NOW() AT TIME ZONE 'UTC'
+            AND jwt_exp > NOW()
         ),
         unique_users AS (
           SELECT
@@ -234,7 +234,7 @@ export class PostgresReportStorageFactory implements storage.ReportStorage {
         AND (
           disconnected_at IS NOT NULL
           OR (
-            jwt_exp < NOW() AT TIME ZONE 'UTC'
+            jwt_exp < NOW()
             AND disconnected_at IS NULL
           )
         );
