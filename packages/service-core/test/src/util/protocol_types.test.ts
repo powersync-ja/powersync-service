f400b0f5783f7c10f30391f5d2825bc267e5fc43
Simon Binder <simon@journeyapps.com>
2025-09-04 18:16:22 +0200

    Allow `null` as stream parameter (#357)
    * Allow `null` as stream parameter
    
    * Additional test

---
+++ packages/service-core/test/src/util/protocol_types.test.ts
@@ -0,0 +1,43 @@
+import { StreamingSyncRequest } from '@/index.js';
+import { schema } from '@powersync/lib-services-framework';
+import { describe, test, expect, it } from 'vitest';
+
+describe('protocol types', () => {
+  describe('StreamingSyncRequest', () => {
+    const validator = schema.createTsCodecValidator(StreamingSyncRequest, { allowAdditional: true });
+
+    test('with streams', () => {
+      expect(
+        validator.validate({
+          buckets: [],
+          include_checksum: true,
+          raw_data: true,
+          binary_data: true,
+          client_id: '0da33a94-c140-4b42-b3b3-a1df3b1352a3',
+          parameters: {},
+          streams: {
+            include_defaults: true,
+            subscriptions: [{ stream: 'does_not_exist', parameters: null, override_priority: null }]
+          }
+        } as any)
+      ).toMatchObject({ valid: true });
+    });
+
+    test('does not allow missing parameters', () => {
+      expect(
+        validator.validate({
+          buckets: [],
+          include_checksum: true,
+          raw_data: true,
+          binary_data: true,
+          client_id: '0da33a94-c140-4b42-b3b3-a1df3b1352a3',
+          parameters: {},
+          streams: {
+            include_defaults: true,
+            subscriptions: [{ stream: 'a', override_priority: null }]
+          }
+        } as any)
+      ).toMatchObject({ valid: false });
+    });
+  });
+});
