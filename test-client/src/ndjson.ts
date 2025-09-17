897b401797b1ecd11bd1b45d790baf501e9aaa57
Ralf Kistner <ralf@journeyapps.com>
2025-09-17 16:56:21 +0200

    Fix error handling for test-client concurrent-connections on http streams.

--- test-client/src/ndjson.ts
+++ test-client/src/ndjson.ts
@@ -10,48 +10,53 @@ export function ndjsonStream<T>(
       var decoder = new TextDecoder();
       var data_buf = '';
 
-      reader.read().then(function processResult(result): void | Promise<any> {
-        if (result.done) {
-          if (cancellationRequest) {
-            // Immediately exit
+      reader
+        .read()
+        .then(function processResult(result): void | Promise<any> {
+          if (result.done) {
+            if (cancellationRequest) {
+              // Immediately exit
+              return;
+            }
+
+            data_buf = data_buf.trim();
+            if (data_buf.length !== 0) {
+              try {
+                var data_l = JSON.parse(data_buf);
+                controller.enqueue({ chunk: data_l, size: data_l.length });
+              } catch (e) {
+                controller.error(e);
+                return;
+              }
+            }
+            controller.close();
             return;
           }
 
-          data_buf = data_buf.trim();
-          if (data_buf.length !== 0) {
-            try {
-              var data_l = JSON.parse(data_buf);
-              controller.enqueue({ chunk: data_l, size: data_l.length });
-            } catch (e) {
-              controller.error(e);
-              return;
+          var data = decoder.decode(result.value, { stream: true });
+          data_buf += data;
+          var lines = data_buf.split('\n');
+          for (var i = 0; i < lines.length - 1; ++i) {
+            var l = lines[i].trim();
+            if (l.length > 0) {
+              try {
+                var data_line = JSON.parse(l);
+                controller.enqueue({ chunk: data_line, size: l.length });
+              } catch (e) {
+                controller.error(e);
+                cancellationRequest = true;
+                reader.cancel();
+                return;
+              }
             }
           }
-          controller.close();
-          return;
-        }
+          data_buf = lines[lines.length - 1];
 
-        var data = decoder.decode(result.value, { stream: true });
-        data_buf += data;
-        var lines = data_buf.split('\n');
-        for (var i = 0; i < lines.length - 1; ++i) {
-          var l = lines[i].trim();
-          if (l.length > 0) {
-            try {
-              var data_line = JSON.parse(l);
-              controller.enqueue({ chunk: data_line, size: l.length });
-            } catch (e) {
-              controller.error(e);
-              cancellationRequest = true;
-              reader.cancel();
-              return;
-            }
-          }
-        }
-        data_buf = lines[lines.length - 1];
-
-        return reader.read().then(processResult);
-      });
+          return reader.read().then(processResult);
+        })
+        .catch((e) => {
+          controller.error(e);
+        });
     },
     cancel: function (reason) {
       cancellationRequest = true;
