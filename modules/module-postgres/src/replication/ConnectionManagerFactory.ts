177eb74c0641015b304c13ae3809287b1341b12b
Ralf Kistner <ralf@journeyapps.com>
2025-11-11 13:41:16 +0200

    Clean up ended ConnectionManagerFactory instances.

--- modules/module-postgres/src/replication/ConnectionManagerFactory.ts
+++ modules/module-postgres/src/replication/ConnectionManagerFactory.ts
@@ -5,7 +5,7 @@ import { logger } from '@powersync/lib-services-framework';
 import { CustomTypeRegistry } from '../types/registry.js';
 
 export class ConnectionManagerFactory {
-  private readonly connectionManagers: PgManager[];
+  private readonly connectionManagers = new Set<PgManager>();
   public readonly dbConnectionConfig: NormalizedPostgresConnectionConfig;
 
   constructor(
@@ -13,18 +13,25 @@ export class ConnectionManagerFactory {
     private readonly registry: CustomTypeRegistry
   ) {
     this.dbConnectionConfig = dbConnectionConfig;
-    this.connectionManagers = [];
   }
 
   create(poolOptions: PgPoolOptions) {
     const manager = new PgManager(this.dbConnectionConfig, { ...poolOptions, registry: this.registry });
-    this.connectionManagers.push(manager);
+    console.log('create ConnectionManagerFactory', this.connectionManagers.size);
+    this.connectionManagers.add(manager);
+
+    manager.registerListener({
+      onEnded: () => {
+        console.log('manager ended');
+        this.connectionManagers.delete(manager);
+      }
+    });
     return manager;
   }
 
   async shutdown() {
     logger.info('Shutting down Postgres connection Managers...');
-    for (const manager of this.connectionManagers) {
+    for (const manager of [...this.connectionManagers]) {
       await manager.end();
     }
     logger.info('Postgres connection Managers shutdown completed.');
