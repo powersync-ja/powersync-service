8fdbf8dc07d53db503c49be79298126931161a1f
Roland Teichert <roland@journeyapps.com>
2025-12-12 09:37:25 +0200

    Moved dynamic module loading to ModuleManager (#436)

--- service/src/runners/unified-runner.ts
+++ service/src/runners/unified-runner.ts
@@ -3,7 +3,6 @@ import * as core from '@powersync/service-core';
 
 import { logBooting } from '../util/version.js';
 import { registerReplicationServices } from './stream-worker.js';
-import { DYNAMIC_MODULES } from '../util/modules.js';
 
 /**
  * Starts an API server
@@ -14,10 +13,6 @@ export const startUnifiedRunner = async (runnerConfig: core.utils.RunnerConfig) 
   const config = await core.utils.loadConfig(runnerConfig);
 
   const moduleManager = container.getImplementation(core.modules.ModuleManager);
-  const modules = await core.loadModules(config, DYNAMIC_MODULES);
-  if (modules.length > 0) {
-    moduleManager.register(modules);
-  }
 
   const serviceContext = new core.system.ServiceContextContainer({
     serviceMode: core.system.ServiceContextMode.UNIFIED,
