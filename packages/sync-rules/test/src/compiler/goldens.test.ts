9eecef1bf514254c5800c6bb17377faf7b5f5ad0
Simon Binder <simon@journeyapps.com>
2026-01-08 14:58:08 +0100

    Port subquery test

--- packages/sync-rules/test/src/compiler/goldens.test.ts
+++ packages/sync-rules/test/src/compiler/goldens.test.ts
@@ -76,6 +76,58 @@ describe('old streams test', () => {
       ).toMatchSnapshot();
     });
   });
+
+  describe('in', () => {
+    test('row value in subquery', () => {
+      expect(
+        compileSingleStreamAndSerialize(
+          'SELECT * FROM comments WHERE issue_id IN (SELECT id FROM issues WHERE owner_id = auth.user_id())'
+        )
+      ).toMatchSnapshot();
+    });
+
+    test('parameter value in subquery', () => {
+      expect(
+        compileSingleStreamAndSerialize(
+          'SELECT * FROM issues WHERE auth.user_id() IN (SELECT id FROM users WHERE is_admin)'
+        )
+      ).toMatchSnapshot();
+    });
+
+    test('two subqueries', () => {
+      expect(
+        compileSingleStreamAndSerialize(
+          `SELECT * FROM users WHERE
+            id IN (SELECT user_a FROM friends WHERE user_b = auth.user_id()) OR
+            id IN (SELECT user_b FROM friends WHERE user_a = auth.user_id())`
+        )
+      ).toMatchSnapshot();
+    });
+
+    test('on parameter data', () => {
+      expect(
+        compileSingleStreamAndSerialize(
+          "SELECT * FROM comments WHERE issue_id IN (subscription.parameters() -> 'issue_id')"
+        )
+      ).toMatchSnapshot();
+    });
+
+    test('on parameter data and table', () => {
+      expect(
+        compileSingleStreamAndSerialize(
+          "SELECT * FROM comments WHERE issue_id IN (SELECT id FROM issues WHERE owner_id = auth.user_id()) AND label IN (subscription.parameters() -> 'labels')"
+        )
+      ).toMatchSnapshot();
+    });
+
+    test('parameter and auth match on same column', () => {
+      expect(
+        compileSingleStreamAndSerialize(
+          "SELECT * FROM comments WHERE issue_id = subscription.parameter('issue') AND issue_id IN auth.parameter('issues')"
+        )
+      ).toMatchSnapshot();
+    });
+  });
 });
 
 // TODO: Replace with parsing from yaml once we support that
