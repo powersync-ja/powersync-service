55fc89e59499de502eecf45a1f2397bfd0630ae1
Simon Binder <simon@journeyapps.com>
2026-01-27 18:38:40 +0100

    Suppport legacy JSON

--- packages/sync-rules/src/sync_plan/engine/sqlite.ts
+++ packages/sync-rules/src/sync_plan/engine/sqlite.ts
@@ -1,3 +1,5 @@
+import { CompatibilityContext, CompatibilityOption } from '../../compatibility.js';
+import { generateSqlFunctions } from '../../index.js';
 import { SqliteValue } from '../../types.js';
 import { ScalarExpressionEngine, ScalarExpressionEvaluator, scalarStatementToSql } from './scalar_expression_engine.js';
 
@@ -7,11 +9,39 @@ import { ScalarExpressionEngine, ScalarExpressionEvaluator, scalarStatementToSql
  * @param module The imported `node:sqlite` module (passed as a parameter to ensure this package keeps working in
  * browsers).
  *
- * @experimental This engine is not drop-in compatible with the JS operator implementations. It also doesn't support
- * legacy JSON behavior. So we can only use this engine when a new compatibility option is enabled.
+ * @experimental This engine is not drop-in compatible with the JS operator implementations. So we can only use this
+ * engine when a new compatibility option is enabled.
  */
-export function nodeSqliteExpressionEngine(module: typeof import('node:sqlite')): ScalarExpressionEngine {
+export function nodeSqliteExpressionEngine(
+  module: typeof import('node:sqlite'),
+  compatibility: CompatibilityContext
+): ScalarExpressionEngine {
   const db = new module.DatabaseSync(':memory:', { readOnly: true, readBigInts: true, returnArrays: true } as any);
+  const functions = generateSqlFunctions(compatibility);
+
+  function registerPowerSyncFunction(name: string) {
+    db.function(name, { useBigIntArguments: true, varargs: true, deterministic: true }, (...args) => {
+      const impl = functions.named[name]!;
+      return impl.call(...args);
+    });
+  }
+
+  // Needed to make them deterministic / prevent passing 'now'
+  registerPowerSyncFunction('unixepoch');
+  registerPowerSyncFunction('datetime');
+
+  registerPowerSyncFunction('ST_AsGeoJSON');
+  registerPowerSyncFunction('AS_AsText');
+  registerPowerSyncFunction('ST_X');
+  registerPowerSyncFunction('ST_Y');
+
+  if (!compatibility.isEnabled(CompatibilityOption.fixedJsonExtract)) {
+    // For backwards compatibility, use the old JSON operators which parse the path argument differently.
+    registerPowerSyncFunction('->');
+    registerPowerSyncFunction('->>');
+    registerPowerSyncFunction('json_extract');
+    registerPowerSyncFunction('json_array_length');
+  }
 
   return {
     prepareEvaluator(input): ScalarExpressionEvaluator {
