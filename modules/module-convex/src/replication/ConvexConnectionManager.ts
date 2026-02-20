6d4e11aac2d5e185b1bc3ca2de907a09dd63b5ce
Kobie Botha <kobie@powersync.com>
2026-02-20 11:28:36 -0700

    use BaseObserver

--- modules/module-convex/src/replication/ConvexConnectionManager.ts
+++ modules/module-convex/src/replication/ConvexConnectionManager.ts
@@ -1,3 +1,4 @@
+import { BaseObserver } from '@powersync/lib-services-framework';
 import { DEFAULT_TAG } from '@powersync/service-sync-rules';
 import { ConvexApiClient } from '../client/ConvexApiClient.js';
 import { ResolvedConvexConnectionConfig } from '../types/types.js';
@@ -6,28 +7,22 @@ export interface ConvexConnectionManagerListener {
   onEnded?: () => void;
 }
 
-export class ConvexConnectionManager {
+export class ConvexConnectionManager extends BaseObserver<ConvexConnectionManagerListener> {
   readonly client: ConvexApiClient;
   readonly schema = 'convex';
   readonly connectionTag: string;
   readonly connectionId: string;
 
-  private listeners = new Set<ConvexConnectionManagerListener>();
-
   constructor(public readonly config: ResolvedConvexConnectionConfig) {
+    super();
     this.client = new ConvexApiClient(config);
     this.connectionTag = config.tag ?? DEFAULT_TAG;
     this.connectionId = config.id ?? 'default';
   }
 
-  registerListener(listener: ConvexConnectionManagerListener) {
-    this.listeners.add(listener);
-  }
-
   async end(): Promise<void> {
-    for (const listener of [...this.listeners]) {
+    this.iterateListeners((listener) => {
       listener.onEnded?.();
-    }
-    this.listeners.clear();
+    });
   }
 }
