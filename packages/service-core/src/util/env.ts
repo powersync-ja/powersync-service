90e234a7725ac20fdd8c6a6ce04c51e7e5c17d79
stevensJourney <steven@journeyapps.com>
2026-02-25 10:17:40 +0200

    fix typo

--- packages/service-core/src/util/env.ts
+++ packages/service-core/src/util/env.ts
@@ -12,7 +12,7 @@ export const env = utils.collectEnvironmentVariables({
    */
   POWERSYNC_CONFIG_B64: utils.type.string.optional(),
   /**
-   * @deprecated use POWERSYNC_CONFIG_B64 instead.
+   * @deprecated use POWERSYNC_SYNC_CONFIG_B64 instead.
    * Base64 encoded contents of sync rules YAML
    */
   POWERSYNC_SYNC_RULES_B64: utils.type.string.optional(),
