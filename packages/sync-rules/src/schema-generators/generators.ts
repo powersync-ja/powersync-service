9daf965dcd4b91da066e688fdd94c4af4f9d274a
Luc de Cafmeyer <luc@powersync.com>
2026-03-12 10:59:58 +0200

    Add .NET class based schema generation (#559)
    * Add .NET class based schema generation
    
    * Add changeset
    
    * PR feedback

--- packages/sync-rules/src/schema-generators/generators.ts
+++ packages/sync-rules/src/schema-generators/generators.ts
@@ -1,5 +1,5 @@
 import { DartSchemaGenerator } from './DartSchemaGenerator.js';
-import { DotNetSchemaGenerator } from './DotNetSchemaGenerator.js';
+import { DotNetSchemaGenerator, DotNetClassSchemaGenerator } from './DotNetSchemaGenerator.js';
 import { JsLegacySchemaGenerator } from './JsLegacySchemaGenerator.js';
 import { KotlinSchemaGenerator } from './KotlinSchemaGenerator.js';
 import { SwiftSchemaGenerator } from './SwiftSchemaGenerator.js';
@@ -8,6 +8,7 @@ import { TsSchemaGenerator, TsSchemaLanguage } from './TsSchemaGenerator.js';
 export const schemaGenerators = {
   dart: new DartSchemaGenerator(),
   dotNet: new DotNetSchemaGenerator(),
+  dotNetClass: new DotNetClassSchemaGenerator(),
   js: new TsSchemaGenerator({ language: TsSchemaLanguage.js }),
   jsLegacy: new JsLegacySchemaGenerator(),
   kotlin: new KotlinSchemaGenerator(),
