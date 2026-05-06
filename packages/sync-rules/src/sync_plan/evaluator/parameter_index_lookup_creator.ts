423822c68afdb1a00ec7f27efe1191b80dc80b28
Simon Binder <simon@journeyapps.com>
2026-05-06 16:49:56 +0200

    Fix replicating parameter rows with multiple outputs (#625)

--- packages/sync-rules/src/sync_plan/evaluator/parameter_index_lookup_creator.ts
+++ packages/sync-rules/src/sync_plan/evaluator/parameter_index_lookup_creator.ts
@@ -3,7 +3,7 @@ import { ParameterIndexLookupCreator } from '../../BucketSource.js';
 import { ParameterLookupScope } from '../../HydrationState.js';
 import { SourceTableInterface } from '../../SourceTableInterface.js';
 import { TablePattern } from '../../TablePattern.js';
-import { SqliteJsonValue, SqliteRow, UnscopedEvaluatedParametersResult } from '../../types.js';
+import { SqliteJsonValue, SqliteParameterValue, SqliteRow, UnscopedEvaluatedParametersResult } from '../../types.js';
 import { ScalarExpressionEvaluator } from '../engine/scalar_expression_engine.js';
 import * as plan from '../plan.js';
 import { StreamEvaluationContext } from './index.js';
@@ -53,8 +53,43 @@ export class PreparedParameterIndexLookupCreator implements ParameterIndexLookup
       return results;
     }
 
+    interface PendingLookup {
+      partitionValues: SqliteParameterValue[];
+      bucketParameters: Record<string, SqliteJsonValue>[];
+    }
+
+    function parametersEqual(a: SqliteParameterValue[], b: SqliteParameterValue[]) {
+      if (a.length != b.length) return false;
+
+      return a.every((val, idx) => val === b[idx]);
+    }
+
     try {
       const inputInstantiation = this.evaluatorInputs.map((input) => row[input.column]);
+
+      // In almost all cases, lookups generate at most one output with exactly one bucket parameter. This is the case
+      // for all lookups without a table-valued function, e.g. `SELECT foo.* FROM foo, bar ON foo.x = bar.x AND
+      // bar.y = auth.user_id()` which generates a lookup from `bar.y` to `bar.x`. However, it's also possible for:
+      //
+      //  - a single row with one partition value to generate multiple outputs, which would yield a lookup with multiple
+      //    entries in bucketParameters.
+      //  - a single row to generate multiple partition values, which would yield multiple lookup entries.
+      //  - a combination of those two, if multiple table-valued functions are used in the parameter result set.
+      const lookups: PendingLookup[] = [];
+
+      function resolveLookup(entries: SqliteParameterValue[]) {
+        // The lookups array could be a hash map, but since we don't expect rows to generate many lookups, a hash map
+        // would likely be more expensive than this linear search.
+        for (const existing of lookups) {
+          if (parametersEqual(existing.partitionValues, entries)) {
+            return existing;
+          }
+        }
+
+        const created: PendingLookup = { partitionValues: entries, bucketParameters: [] };
+        lookups.push(created);
+        return created;
+      }
 
       for (const outputRow of this.evaluator.evaluate(inputInstantiation)) {
         if (!isValidParameterValueRow(outputRow)) {
@@ -68,8 +103,12 @@ export class PreparedParameterIndexLookupCreator implements ParameterIndexLookup
 
         // source is [...outputs, ...partitionValues]
         const partitionValues = outputRow.splice(this.numberOfOutputs, this.numberOfParameters);
-        const lookup = UnscopedParameterLookup.normalized(partitionValues);
-        results.push({ lookup, bucketParameters: [outputs] });
+        const pendingLookup = resolveLookup(partitionValues);
+        pendingLookup.bucketParameters.push(outputs);
+      }
+
+      for (const { partitionValues, bucketParameters } of lookups) {
+        results.push({ lookup: UnscopedParameterLookup.normalized(partitionValues), bucketParameters });
       }
     } catch (e) {
       results.push({ error: e.message });
