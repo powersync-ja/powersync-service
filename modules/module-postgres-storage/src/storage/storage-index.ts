2524ea375435f6a003afa4aa48fff983ab084fb2
Ralf Kistner <ralf@journeyapps.com>
2026-06-18 14:48:08 +0200

    Simplify package exports and tsconfig.

--- modules/module-postgres-storage/src/storage/storage-index.ts
+++ modules/module-postgres-storage/src/storage/storage-index.ts
@@ -1,4 +1,5 @@
 export * from './PostgresBucketStorageFactory.js';
 export * from './PostgresCompactor.js';
+export * from './PostgresReportStorage.js';
 export * from './PostgresStorageProvider.js';
 export * from './PostgresSyncRulesStorage.js';
