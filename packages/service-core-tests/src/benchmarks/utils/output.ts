e441c5cc5552202d00d5a8cc2dfc8319ddb0a2e1
Ry Anderson <60347977+Omnicpie@users.noreply.github.com>
2026-08-17 16:43:00 +0100

    Add outputs util file

---
+++ packages/service-core-tests/src/benchmarks/utils/output.ts
@@ -0,0 +1,11 @@
+import { mkdir } from 'node:fs/promises';
+
+export const getArtifactFilename = (runId: string) => {
+  return `./benchmark-artifacts/json/${runId}.json`;
+};
+
+export const createArtifactsFolder = async () => {
+  try {
+    await mkdir('./benchmark-artifacts/json', { recursive: true });
+  } catch {}
+};
